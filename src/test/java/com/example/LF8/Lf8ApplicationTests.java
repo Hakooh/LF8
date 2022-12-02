cf1f5b4c91e7d21d2c57917f2084a2fd12cf0ae4
Kevin <kevin.heinitz@kuehne-nagel.com>
2022-12-02 12:39:48 +0100

    maven setup

---
+++ src/test/java/com/example/LF8/Lf8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LF8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lf8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
