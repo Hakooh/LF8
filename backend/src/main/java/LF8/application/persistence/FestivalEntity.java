3f83f8c6f34711a8ccbff2e69f49306aff081021
Kevin.Heinitz <kevin.heinitz@kuehne-nagel.com>
2022-12-08 11:42:15 +0100

    Added JPA dependency and initialized database

--- backend/src/main/java/LF8/application/persistence/FestivalEntity.java
+++ backend/src/main/java/LF8/application/persistence/FestivalEntity.java
@@ -1,20 +1,39 @@
 package LF8.application.persistence;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class FestivalEntity {
+    @Id
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String festivalPlace;
+
+    @Column
     private String address;
+
+    @Column
     private LocalDate startDate;
+
+    @Column
     private LocalDate endDate;
+
+    @OneToMany
     private Set<ShopEntity> shops;
 
 }
