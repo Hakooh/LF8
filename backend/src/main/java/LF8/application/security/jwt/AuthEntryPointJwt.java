bb1523ffd5e6cc8bc523912a4675829386559658
vivien-laasch <vivien.laasch@outlook.com>
2022-12-14 21:38:50 +0100

    cleaned imports

--- backend/src/main/java/LF8/application/security/jwt/AuthEntryPointJwt.java
+++ backend/src/main/java/LF8/application/security/jwt/AuthEntryPointJwt.java
@@ -1,7 +1,5 @@
 package LF8.application.security.jwt;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
