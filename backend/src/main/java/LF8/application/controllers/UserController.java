b5e040c5dba917eb9ec1bc053d9a9b4ba3b6678d
vivien-laasch <vivien.laasch@outlook.com>
2022-12-21 10:59:37 +0100

    Revert "added GetMappings to UserController"
    This reverts commit 54475f38c26f203b678af18524b5d44c40a27aec.

--- backend/src/main/java/LF8/application/controllers/UserController.java
+++ backend/src/main/java/LF8/application/controllers/UserController.java
@@ -2,7 +2,10 @@ package LF8.application.controllers;
 
 import LF8.application.persistence.UserEntity;
 import LF8.application.persistence.UserEntityRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -30,30 +33,10 @@ public class UserController {
         }
     }
 
-    @GetMapping("/firstname/{firstName}/lastname/{lastName}")
-    public UserEntity findUserByFullName(@PathVariable String firstName, @PathVariable String lastName) {
-        Optional<UserEntity> userEntityOptional = userEntityRepository.findByFirstNameAndLastName(firstName, lastName);
-        if (userEntityOptional.isEmpty()) {
-            return null;
-        } else {
-            return userEntityOptional.get();
-        }
-    }
-
-    @GetMapping("/email/{eMail}")
-    public UserEntity findUserByeMail(@PathVariable String eMail) {
-        Optional<UserEntity> userEntityOptional = userEntityRepository.findByeMail(eMail);
-        if (userEntityOptional.isEmpty()) {
-            return null;
-        } else {
-            return userEntityOptional.get();
-        }
-    }
-
     @DeleteMapping("/delete/id/{id}")
     public void deleteUser(@PathVariable Long id) {userEntityRepository.deleteById(id);}
 
-    @PostMapping("/add")
+    @PostMapping()
     public UserEntity addUser(@RequestBody UserEntity userEntity) {
         Optional<UserEntity> existingUser = userEntityRepository
                 .findByFirstNameAndLastName(userEntity.getFirstName(), userEntity.getLastName());
@@ -62,5 +45,9 @@ public class UserController {
         } else {
             return existingUser.get();
         }
+
+    public UserEntity findByeMail(String eMail) {
+        return userEntityRepository.findByeMail(eMail).get();
+    }
 
 }
